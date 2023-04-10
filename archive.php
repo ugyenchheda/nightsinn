3881fa2429ae41f7ee490174b452f2e1309f2e8c
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-04-10 21:48:02 +0300

    Update archive.php

--- archive.php
+++ archive.php
@@ -16,34 +16,22 @@
 	//Reset Query
 	wp_reset_query();
 	
-	
-	
-/* ------------------------------------------------
-	Get Page Title
------------------------------------------------- */
-
-	// Category
 	if (is_category()) :
 		$page_title = sprintf( __('All posts in: "%s"', 'qns'), single_cat_title('',false) );
 
-	// Tag
 	elseif (is_tag()) :
 		$page_title = sprintf( __('All posts tagged: "%s"', 'qns'), single_tag_title('',false) );
 
-	// Author
 	elseif ( is_author() ) :	
 		$userdata = get_userdata($author);
 		$page_title = sprintf( __('All posts by: "%s"', 'qns'), $userdata->display_name );
 
-	// Day
 	elseif ( is_day() ) :
 		$page_title = sprintf( __( 'Daily Archives: <span>%s</span>', 'qns' ), get_the_date() );
-	
-	// Month	
+		
 	elseif ( is_month() ) :
 		$page_title = sprintf( __( 'Monthly Archives: <span>%s</span>', 'qns' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'qns' ) ) );
 	
-	// Year
 	elseif ( is_year() ) :
 		$page_title = sprintf( __( 'Yearly Archives: <span>%s</span>', 'qns' ), get_the_date( _x( 'Y', 'yearly archives date format', 'qns' ) ) );
 	
