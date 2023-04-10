12913371e9af2bace87566db2b554c93225320d3
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-04-10 21:49:12 +0300

    Update single-testimonial.php

--- single-testimonial.php
+++ single-testimonial.php
@@ -14,37 +14,25 @@
 	
 	// Get Content ID/Class
 	$content_id_class = content_id_class(get_post_meta($post_id, 'qns_page_sidebar', true));
-	
 	// Reset Query
 	wp_reset_query();
-
 ?>
-
 <?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	
 	<?php
 		$testimonial_guest = get_post_meta($post->ID, $prefix.'testimonial_guest', true);
 		$testimonial_room = get_post_meta($post->ID, $prefix.'testimonial_room', true);
 	?>
-
 <div id="page-header" <?php echo $header_image; ?>>
 	<h2><?php echo $testimonial_guest; ?> - <span><?php echo $testimonial_room; ?></h2>
 </div>
-
 <?php endwhile;endif; ?>
-
 <!-- BEGIN .content-wrapper -->
 <div class="content-wrapper clearfix">
-	
 	<!-- BEGIN .main-content -->
 	<div class="<?php echo $content_id_class; ?> page-content">
-		
 		<?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		
 			<div class="testimonial-wrapper testimonial-single clearfix">
-					
 				<div class="testimonial-image">
-					
 				<?php				
 					if(has_post_thumbnail()) {
 						$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'image-style2' );
@@ -55,20 +43,13 @@
 					}			
 				?>
 				</div>
-
 				<div class="testimonial-text"><?php the_content(); ?></div>
 				<div class="testimonial-speech"></div>
-		
 			</div>
-		
 		<?php endwhile;endif; ?>
-		
 	<!-- END .main-content -->	
 	</div>
-
 	<?php get_sidebar(); ?>
-
 <!-- END .content-wrapper -->
 </div>
-
 <?php get_footer(); ?>
